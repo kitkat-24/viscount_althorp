8c3825090543549a5ca12f2e28e05d56cc709cf4
William Ruppenthal <williamruppenthal42@gmail.com>
2019-03-10 14:42:00 -0400

    Updated justification to show days taken to discover

--- cogs/rolls.py
+++ cogs/rolls.py
@@ -15,11 +15,11 @@ class Rolls(commands.Cog, name="Stat Commands"):
     @commands.has_permissions(administrator=True)
     async def justify(self, ctx, dur : int, maxinf : int):
         """Returns infamy gained due to war goal justification."""
-        days = np.random.normal(dur/2, dur*0.375)
+        days = round(np.random.normal(dur/2, dur*0.375))
         inf = round(maxinf*(days/dur))
 
         if inf > 0:
-            await ctx.send('Discovered! Infamy accrued: {}'.format(inf))
+            await ctx.send('Discovered in {} days! Infamy accrued: {}'.format(days, inf))
         else:
             await ctx.send('Fabrication not discovered.')
 
